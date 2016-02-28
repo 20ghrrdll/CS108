fcff7e341ce1352d764fc4e8d6b7331fabcb5597
Regina Nguyen <renguyen@stanford.edu>
2016-02-27 17:16:03 -0800

    started quizmanager

---
+++ QuizWhiz/src/quizzes/Quiz.java
@@ -0,0 +1,9 @@
+package quizzes;
+
+public class Quiz {
+
+	private int id;
+	private String name, description, creator;
+	private boolean practiceMode;
+	
+}
--- QuizWhiz/src/quizzes/QuizManager.java
+++ QuizWhiz/src/quizzes/QuizManager.java
@@ -2,14 +2,38 @@ package quizzes;
 
 import main.DBConnector;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class QuizManager {
 
-	private DBConnector connector;
-	private Connection con;
+	private static Connection con = null;
 	
 	public QuizManager() {
 		
+	}
+	
+	/**
+	 * Returns Quiz objects for every quiz currently stored in the database.
+	 * @return ArrayList of Quiz objects
+	 */
+	public ArrayList<Quiz> getAllQuizzes() {
+		ArrayList<Quiz> quizzes = new ArrayList<Quiz>();
+		try {
+			con = DBConnector.getConnection();
+			Statement stmt = con.createStatement();
+			String query = "SELECT * FROM " + "quiz";
+			ResultSet rs = stmt.executeQuery(query);
+			
+			while (rs.next()) {
+				int quizID = rs.getInt("quizID");
+				String name = rs.getString("name");
+			}
+			DBConnector.closeConnection();
+		} catch (SQLException e) {
+			e.printStackTrace(); // TODO: what to do here
+		}
+		
+		return quizzes;
 	}
 	
 }
