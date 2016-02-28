fdb8a0f82f363cb6fd1880dab43a114122b2248f
Regina Nguyen <renguyen@stanford.edu>
2016-02-27 16:28:50 -0800

    starting quizmanager

---
+++ QuizWhiz/src/quizzes/QuizManager.java
@@ -0,0 +1,15 @@
+package quizzes;
+
+import main.DBConnector;
+import java.sql.*;
+
+public class QuizManager {
+
+	private DBConnector connector;
+	private Connection con;
+	
+	public QuizManager() {
+		
+	}
+	
+}
