e7f3802e45e802fc8d9722619b2e5ff6c08cd5fb
Regina Nguyen <renguyen@stanford.edu>
2016-03-09 20:41:19 -0800

    Added creation achievements

--- QuizWhiz/src/quizzes/QuizManager.java
+++ QuizWhiz/src/quizzes/QuizManager.java
@@ -147,25 +147,28 @@ public class QuizManager {
 	 * @return ArrayList of recently created quizzes
 	 * @throws SQLException
 	 */
-	public ArrayList<Quiz> getRecentlyCreatedQuizzes() throws SQLException {
+	public ArrayList<Quiz> getRecentlyCreatedQuizzes() {
 		ArrayList<Quiz> quizzes = new ArrayList<Quiz>();
-		Statement stmt = con.createStatement();		
-		String query = "SELECT * FROM " + MyDBInfo.QUIZ_TABLE +" ORDER BY created DESC;";
-		ResultSet rs = stmt.executeQuery(query);
-
-		while (rs.next()) {
-			int quizID = rs.getInt("quizID");
-			String name = rs.getString("name");
-			String description = rs.getString("description");
-			Timestamp created = rs.getTimestamp("created");
-			String creator = rs.getString("creatorId");
-			QuizType type = QuizType.valueOf(rs.getString("type"));
-			boolean practiceMode = rs.getBoolean("practiceMode");
-			boolean multiplePages = rs.getBoolean("pages");
-			boolean random = rs.getBoolean("random");
-			boolean immediateCorrection = rs.getBoolean("correction");
-			Quiz quiz = new Quiz(quizID, name, description, created, creator, type, practiceMode, multiplePages, random, immediateCorrection);
-			quizzes.add(quiz);
+		try {
+			Statement stmt = con.createStatement();		
+			String query = "SELECT * FROM " + MyDBInfo.QUIZ_TABLE +" ORDER BY created DESC;";
+			ResultSet rs = stmt.executeQuery(query);
+	
+			while (rs.next()) {
+				int quizID = rs.getInt("quizID");
+				String name = rs.getString("name");
+				String description = rs.getString("description");
+				Timestamp created = rs.getTimestamp("created");
+				String creator = rs.getString("creatorId");
+				QuizType type = QuizType.valueOf(rs.getString("type"));
+				boolean practiceMode = rs.getBoolean("practiceMode");
+				boolean multiplePages = rs.getBoolean("pages");
+				boolean random = rs.getBoolean("random");
+				boolean immediateCorrection = rs.getBoolean("correction");
+				Quiz quiz = new Quiz(quizID, name, description, created, creator, type, practiceMode, multiplePages, random, immediateCorrection);
+				quizzes.add(quiz);
+			}
+		} catch (SQLException e) {
 		}
 		return quizzes;
 	}
@@ -176,16 +179,19 @@ public class QuizManager {
 	 * @return ArrayList of recently taken quizzes
 	 * @throws SQLException
 	 */
-	public ArrayList<Quiz> getRecentlyTakenQuizzes() throws SQLException {
+	public ArrayList<Quiz> getRecentlyTakenQuizzes() {
 		ArrayList<Quiz> quizzes = new ArrayList<Quiz>();
-		Statement stmt = con.createStatement();		
-		String query = "SELECT * FROM " + MyDBInfo.QUIZ_RECORDS_TABLE +" ORDER BY end_time DESC;";
-		ResultSet rs = stmt.executeQuery(query);
-
-		while (rs.next()) {
-			int quizID = rs.getInt("quizID");
-			Quiz quiz = getQuiz(quizID);
-			quizzes.add(quiz);
+		try {
+			Statement stmt = con.createStatement();		
+			String query = "SELECT * FROM " + MyDBInfo.QUIZ_RECORDS_TABLE +" ORDER BY end_time DESC;";
+			ResultSet rs = stmt.executeQuery(query);
+	
+			while (rs.next()) {
+				int quizID = rs.getInt("quizID");
+				Quiz quiz = getQuiz(quizID);
+				quizzes.add(quiz);
+			}
+		} catch (SQLException e) {
 		}
 		return quizzes;
 	}
@@ -197,127 +203,142 @@ public class QuizManager {
 	 * @return ArrayList of quizzes created by a given user
 	 * @throws SQLException
 	 */
-	public ArrayList<Quiz> getMyQuizzes(String username) throws SQLException {
+	public ArrayList<Quiz> getMyQuizzes(String username)  {
 		ArrayList<Quiz> quizzes = new ArrayList<Quiz>();
-		Statement stmt = con.createStatement();
-		String query = "SELECT * FROM " + MyDBInfo.QUIZ_TABLE +" WHERE creatorId='"+username +"';";
-		ResultSet rs = stmt.executeQuery(query);
-
-		while (rs.next()) {
-			int quizID = rs.getInt("quizID");
-			String name = rs.getString("name");
-			String description = rs.getString("description");
-			Timestamp created = rs.getTimestamp("created");
-			String creator = rs.getString("creatorId");
-			QuizType type = QuizType.valueOf(rs.getString("type"));
-			boolean practiceMode = rs.getBoolean("practiceMode");
-			boolean multiplePages = rs.getBoolean("pages");
-			boolean random = rs.getBoolean("random");
-			boolean immediateCorrection = rs.getBoolean("correction");
-			Quiz quiz = new Quiz(quizID, name, description, created, creator, type, practiceMode, multiplePages, random, immediateCorrection);
-			quizzes.add(quiz);
+		try {
+			Statement stmt = con.createStatement();
+			String query = "SELECT * FROM " + MyDBInfo.QUIZ_TABLE +" WHERE creatorId='"+username +"';";
+			ResultSet rs = stmt.executeQuery(query);
+	
+			while (rs.next()) {
+				int quizID = rs.getInt("quizID");
+				String name = rs.getString("name");
+				String description = rs.getString("description");
+				Timestamp created = rs.getTimestamp("created");
+				String creator = rs.getString("creatorId");
+				QuizType type = QuizType.valueOf(rs.getString("type"));
+				boolean practiceMode = rs.getBoolean("practiceMode");
+				boolean multiplePages = rs.getBoolean("pages");
+				boolean random = rs.getBoolean("random");
+				boolean immediateCorrection = rs.getBoolean("correction");
+				Quiz quiz = new Quiz(quizID, name, description, created, creator, type, practiceMode, multiplePages, random, immediateCorrection);
+				quizzes.add(quiz);
+			}
+		} catch (SQLException e) {
 		}
 		return quizzes;
 	}
 
-	public ArrayList<Quiz> getMyRecentQuizzes(String username) throws SQLException {
+	public ArrayList<Quiz> getMyRecentQuizzes(String username) {
 		ArrayList<Quiz> quizzes = new ArrayList<Quiz>();
-		Statement stmt = con.createStatement();		
-		String query = "SELECT * FROM " + MyDBInfo.QUIZ_TABLE +" WHERE creatorId='"+username +"' ORDER BY 'end_time' DESC;";
-		ResultSet rs = stmt.executeQuery(query);
-
-		while (rs.next()) {
-			int quizID = rs.getInt("quizID");
-			String name = rs.getString("name");
-			String description = rs.getString("description");
-			Timestamp created = rs.getTimestamp("created");
-			String creator = rs.getString("creatorId");
-			QuizType type = QuizType.valueOf(rs.getString("type"));
-			boolean practiceMode = rs.getBoolean("practiceMode");
-			boolean multiplePages = rs.getBoolean("pages");
-			boolean random = rs.getBoolean("random");
-			boolean immediateCorrection = rs.getBoolean("correction");
-			Quiz quiz = new Quiz(quizID, name, description, created, creator, type, practiceMode, multiplePages, random, immediateCorrection);
-			quizzes.add(quiz);
+		try {
+			Statement stmt = con.createStatement();		
+			String query = "SELECT * FROM " + MyDBInfo.QUIZ_TABLE +" WHERE creatorId='"+username +"' ORDER BY 'end_time' DESC;";
+			ResultSet rs = stmt.executeQuery(query);
+	
+			while (rs.next()) {
+				int quizID = rs.getInt("quizID");
+				String name = rs.getString("name");
+				String description = rs.getString("description");
+				Timestamp created = rs.getTimestamp("created");
+				String creator = rs.getString("creatorId");
+				QuizType type = QuizType.valueOf(rs.getString("type"));
+				boolean practiceMode = rs.getBoolean("practiceMode");
+				boolean multiplePages = rs.getBoolean("pages");
+				boolean random = rs.getBoolean("random");
+				boolean immediateCorrection = rs.getBoolean("correction");
+				Quiz quiz = new Quiz(quizID, name, description, created, creator, type, practiceMode, multiplePages, random, immediateCorrection);
+				quizzes.add(quiz);
+			}
+		} catch (SQLException e) {
 		}
 		return quizzes;
 	}
 
-	public ArrayList<Quiz> getMyRecentlyTakenQuizzes(String username) throws SQLException {
+	public ArrayList<Quiz> getMyRecentlyTakenQuizzes(String username) {
 		ArrayList<Quiz> quizzes = new ArrayList<Quiz>();
-		Statement stmt = con.createStatement();		
-		String query = "SELECT * FROM " + MyDBInfo.QUIZ_RECORDS_TABLE +" WHERE userId='" + username + "' ORDER BY end_time DESC;";
-		ResultSet rs = stmt.executeQuery(query);
-
-		while (rs.next()) {
-			int quizID = rs.getInt("quizID");
-			Quiz quiz = getQuiz(quizID);
-			quizzes.add(quiz);
+		try {
+			Statement stmt = con.createStatement();		
+			String query = "SELECT * FROM " + MyDBInfo.QUIZ_RECORDS_TABLE +" WHERE userId='" + username + "' ORDER BY end_time DESC;";
+			ResultSet rs = stmt.executeQuery(query);
+	
+			while (rs.next()) {
+				int quizID = rs.getInt("quizID");
+				Quiz quiz = getQuiz(quizID);
+				quizzes.add(quiz);
+			}
+		} catch (SQLException e) {
 		}
 		return quizzes;
 	}
 
-	public ArrayList<QuizPerformance> getMyRecentlyTakenQuizPerformance (String username, int quizId) throws SQLException {
+	public ArrayList<QuizPerformance> getMyRecentlyTakenQuizPerformance (String username, int quizId) {
 		ArrayList<QuizPerformance> recentlyTaken = new ArrayList<QuizPerformance>();
-		Statement stmt = con.createStatement();		
-		String query = "SELECT * FROM " + MyDBInfo.QUIZ_RECORDS_TABLE +" WHERE quizId='" + quizId + "' AND userId='"+ username + "' ORDER BY end_time DESC;";
-		System.out.println("got here");
-		System.out.println(query);
-		ResultSet rs = stmt.executeQuery(query);
-		while (rs.next()) {
-			String name = rs.getString("userId");
-			int score = rs.getInt("score");
-			Timestamp start_time = rs.getTimestamp("start_time");
-			Timestamp end_time = rs.getTimestamp("end_time");
-
-			recentlyTaken.add(new QuizPerformance(name, score, start_time, end_time));
+		try {
+			Statement stmt = con.createStatement();		
+			String query = "SELECT * FROM " + MyDBInfo.QUIZ_RECORDS_TABLE +" WHERE quizId='" + quizId + "' AND userId='"+ username + "' ORDER BY end_time DESC;";
+			System.out.println("got here");
+			System.out.println(query);
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String name = rs.getString("userId");
+				int score = rs.getInt("score");
+				Timestamp start_time = rs.getTimestamp("start_time");
+				Timestamp end_time = rs.getTimestamp("end_time");
+	
+				recentlyTaken.add(new QuizPerformance(name, score, start_time, end_time));
+			}
+		} catch (SQLException e) {
 		}
-
 		return recentlyTaken;	
 	}
 
 
 
-	public ArrayList<QuizPerformance> getQuizHighScores(int quizId) throws SQLException {
+	public ArrayList<QuizPerformance> getQuizHighScores(int quizId) {
 		ArrayList<QuizPerformance> highScores = new ArrayList<QuizPerformance>();
-		Statement stmt = con.createStatement();		
-		String query = "SELECT * FROM " + MyDBInfo.QUIZ_RECORDS_TABLE +" WHERE quizId='" + quizId + "' ORDER BY score DESC;";
-
-		System.out.println(query);
-		ResultSet rs = stmt.executeQuery(query);
-		while (rs.next()) {
-			String name = rs.getString("userId");
-			int score = rs.getInt("score");
-			Timestamp start_time = rs.getTimestamp("start_time");
-			Timestamp end_time = rs.getTimestamp("end_time");
-
-			highScores.add(new QuizPerformance(name, score, start_time, end_time));
+		try {
+			Statement stmt = con.createStatement();		
+			String query = "SELECT * FROM " + MyDBInfo.QUIZ_RECORDS_TABLE +" WHERE quizId='" + quizId + "' ORDER BY score DESC;";
+	
+			System.out.println(query);
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String name = rs.getString("userId");
+				int score = rs.getInt("score");
+				Timestamp start_time = rs.getTimestamp("start_time");
+				Timestamp end_time = rs.getTimestamp("end_time");
+	
+				highScores.add(new QuizPerformance(name, score, start_time, end_time));
+			}
+		} catch (SQLException e) {
 		}
-
 		return highScores;
 	} 
 
-	public ArrayList<QuizPerformance> getQuizPerformances(int quizId) throws SQLException {
+	public ArrayList<QuizPerformance> getQuizPerformances(int quizId) {
 		ArrayList<QuizPerformance> highScores = new ArrayList<QuizPerformance>();
-		Statement stmt = con.createStatement();		
-		String query = "SELECT * FROM " + MyDBInfo.QUIZ_RECORDS_TABLE +" WHERE quizId='" + quizId + "' ORDER BY end_time DESC;";
-
-		System.out.println(query);
-		ResultSet rs = stmt.executeQuery(query);
-		while (rs.next()) {
-			String name = rs.getString("userId");
-			int score = rs.getInt("score");
-			Timestamp start_time = rs.getTimestamp("start_time");
-			Timestamp end_time = rs.getTimestamp("end_time");
-
-			highScores.add(new QuizPerformance(name, score, start_time, end_time));
+		try {
+			Statement stmt = con.createStatement();		
+			String query = "SELECT * FROM " + MyDBInfo.QUIZ_RECORDS_TABLE +" WHERE quizId='" + quizId + "' ORDER BY end_time DESC;";
+	
+			System.out.println(query);
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String name = rs.getString("userId");
+				int score = rs.getInt("score");
+				Timestamp start_time = rs.getTimestamp("start_time");
+				Timestamp end_time = rs.getTimestamp("end_time");
+	
+				highScores.add(new QuizPerformance(name, score, start_time, end_time));
+			}
+		} catch (SQLException e) {
 		}
-
 		return highScores;
 	} 
 
 
-	public void insertQuiz(Quiz quiz) {
+	public boolean insertQuiz(Quiz quiz) {
 		/* String query = "INSERT INTO " + MyDBInfo.QUIZ_TABLE + " (name, description, created, creatorId"
 				+ ", practiceMode, pages, random, correction, type, amountTaken)" + 
 				" VALUES ("+ '"+name+"' + ", " + description + ", " + created + ", " + creatorId + ", " + practice + ", " +
@@ -342,9 +363,9 @@ public class QuizManager {
 			System.out.println(s.toString());
 			s.executeUpdate();
 		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			return false;
 		}
+		return true;
 	}
 
 	public void closeConnection() {
